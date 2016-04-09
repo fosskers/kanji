f52c766ab1a663214ec29c269d582f79471308c9
Colin Woodbury <colingw@gmail.com>
2016-04-08 21:43:32 -0700

    (nanq-site) Cleaned out dummy code

--- nanq-site/README.md
+++ nanq-site/README.md
@@ -1,3 +1,27 @@
 nanq.ca
 =======
 
+This is a website for learners and native speakers of Japanese to analyse
+bodies of Japanese text for difficultly. Here we measure difficulty by
+a number of factors:
+
+* Density of Kanji vs non-Kanji in the text
+* Density of Kanji learned in Japanese elementary school within the text
+* Appearance of Kanji from higher levels of the [Japan Kanji Aptitude Test]()
+(Kanken)
+
+Want to read a book or manga, and would like to know up-front what Kanji
+you'll need to learn?
+
+Want to be better prepared for your next Kanken?
+
+Interested in finding out how hard a "hard" text really is?
+
+[Then head over to the site]()
+
+*This website is written completely in [Haskell](haskell.org) and is
+powered by the [servant]() and [kanji]() libraries.*
+
+Notes
+-----
+- Use `vegeta` for HTTP load testing
--- nanq-site/nanq-site.hs
+++ nanq-site/nanq-site.hs
@@ -1,4 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Main where
 
+import Data.Aeson
+import Data.List
+import GHC.Generics
+import Lucid
+import Network.HTTP.Media
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.HTML.Lucid
+
+---
+
+type PersonAPI = "persons" :> Get '[JSON, HTML] [Person]
+
+data Person = Person { firstName :: String, lastName :: String } deriving Generic
+
+instance ToJSON Person
+
+instance ToHtml Person where
+  toHtml person = tr_ $ do
+    td_ (toHtml $ firstName person)
+    td_ (toHtml $ lastName person)
+
+  toHtmlRaw = toHtml
+
+instance ToHtml [Person] where
+  toHtml ps = table_ $ do
+    tr_ $ th_ "first name" *> th_ "last name"
+    foldMap toHtml ps
+
+  toHtmlRaw = toHtml
+
+persons :: [Person]
+persons = [ Person "Colin" "Woodbury"
+          , Person "Jack" "Catworthy" ]
+
+personAPI :: Proxy PersonAPI
+personAPI = Proxy
+
+server :: Server PersonAPI
+server = pure persons
+
+app :: Application
+app = serve personAPI server
+
 main :: IO ()
-main = pure ()
+main = run 8081 app

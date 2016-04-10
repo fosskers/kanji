b9374365ce690f44516a938bf113dc00c36e88a0
Colin Woodbury <colingw@gmail.com>
2016-04-10 15:56:32 -0700

    (site) Linting

--- nanq-site/nanq-site.hs
+++ nanq-site/nanq-site.hs
@@ -7,67 +7,35 @@
 
 module Main where
 
-import Data.Aeson
-import Data.List
-import GHC.Generics
-import Lucid
-import Network.HTTP.Media
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Servant
-import Servant.HTML.Lucid
-import Pages.Pages
 import qualified Data.Text as T
+import           Lucid
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Pages.Pages
+import           Servant
+import           Servant.HTML.Lucid
 
 ---
 
---"persons" :> Get '[JSON, HTML] [Person]
-type API = "analyse" :> ReqBody '[FormUrlEncoded] [(T.Text,T.Text)] :> Post '[HTML] (Html ())
+-- | All endpoints for our server.
+type API = "analyse" :> ReqBody '[FormUrlEncoded] [(T.Text,T.Text)]
+                     :> Post '[HTML] (Html ())
            :<|> Get '[HTML] (Html ())
            :<|> "assets" :> Raw
 
---data Jap = Jap { japText :: TL.Text } deriving Generic
+main :: IO ()
+main = run 8081 app
 
---instance FromJSON Jap
-
-{-}
-data Person = Person { firstName :: String
-                     , lastName :: String } deriving Generic
-
-instance ToJSON Person
-
-instance ToHtml Person where
-  toHtml person = tr_ $ do
-    td_ (toHtml $ firstName person)
-    td_ (toHtml $ lastName person)
-
-  toHtmlRaw = toHtml
-
-instance ToHtml [Person] where
-  toHtml ps = table_ $ do
-    tr_ $ th_ "first name" *> th_ "last name"
-    foldMap toHtml ps
-
-  toHtmlRaw = toHtml
-
-persons :: [Person]
-persons = [ Person "Colin" "Woodbury"
-          , Person "Jack" "Catworthy" ]
--}
+app :: Application
+app = serve api server
 
 api :: Proxy API
 api = Proxy
 
 server :: Server API
-server = (pure . foo) :<|> pure (base home) :<|> serveDirectory "assets"
+server = (pure . formInput) :<|> pure (base home) :<|> serveDirectory "assets"
 
-app :: Application
-app = serve api server
-
-main :: IO ()
-main = run 8081 app
-
-foo :: [(T.Text,T.Text)] -> Html ()
-foo [] = base $ p_ "You didn't give any input!"
-foo ((_,""):_) = base $ p_ "You didn't give any input!"
-foo ((_,t):_) = base $ p_ (toHtml t)
+formInput :: [(T.Text,T.Text)] -> Html ()
+formInput [] = base $ p_ "You didn't give any input!"
+formInput ((_,""):_) = base $ p_ "You didn't give any input!"
+formInput ((_,t):_) = base $ p_ (toHtml t)

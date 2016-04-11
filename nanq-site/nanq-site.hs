0ce5df61aa5a8886dff236500b76e575695249fd
Colin Woodbury <colingw@gmail.com>
2016-04-10 21:42:53 -0700

    (site) Crudely display analysis info

--- nanq-site/nanq-site.hs
+++ nanq-site/nanq-site.hs
@@ -4,9 +4,11 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ViewPatterns  #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Main where
 
+import           Data.Kanji
 import qualified Data.Text as T
 import           Lucid
 import           Network.Wai
@@ -14,6 +16,9 @@ import           Network.Wai.Handler.Warp
 import           Pages.Pages
 import           Servant
 import           Servant.HTML.Lucid
+import           Text.Printf.TH
+import qualified Data.Map.Lazy as M
+import qualified Data.Set as S
 
 ---
 
@@ -33,9 +38,44 @@ api :: Proxy API
 api = Proxy
 
 server :: Server API
-server = (pure . formInput) :<|> pure (base home) :<|> serveDirectory "assets"
+server = (pure . analyse) :<|> pure (base home) :<|> serveDirectory "assets"
 
-formInput :: [(T.Text,T.Text)] -> Html ()
-formInput [] = base $ p_ "You didn't give any input!"
-formInput ((_,""):_) = base $ p_ "You didn't give any input!"
-formInput ((_,t):_) = base $ p_ (toHtml t)
+analyse :: [(T.Text,T.Text)] -> Html ()
+analyse [] = base $ p_ "You didn't give any input!"
+analyse ((_,""):_) = base $ p_ "You didn't give any input!"
+analyse ((_,t):_) = base $ do
+  p_ . toHtml $ [lt|Elementary Kanji: %.2f%%|] (100 * e)
+  p_ . toHtml $ [lt|Kanji Density: %.2f%%|] (100 * d)
+  p_ . toHtml $ [lt|Average Level: %.2f|] a
+  h3_ "Level Densities"
+  p_ . dist $ levelDist ks
+  h3_ "Kanji per Level"
+  p_ $ splits ks
+  h3_ "Unknown Kanji"
+  p_ . toHtml $ unknowns ks
+    where ks = asKanji t
+          e = elementaryKanjiDensity ks
+          d = kanjiDensity (T.length t) ks
+          a = averageLevel ks
+
+-- | Convert "Level Distribution" results into a table.
+dist :: [(Rank,Float)] -> Html ()
+dist = table_ [class_ "table"] . mconcat . map f
+  where f :: (Rank,Float) -> Html ()
+        f (r,n) = tr_ $ do
+          td_ . toHtml $ show r
+          td_ . toHtml $ [lt|%.2f%%|] (100 * n)
+
+-- | Show the `Kanji` that appeared in each Level as a table.
+splits :: [Kanji] -> Html ()
+splits = table_ [class_ "table"] . mconcat . map f . g
+  where g = M.toList . S.foldl h M.empty . S.fromList
+        h a k = maybe a (\l -> M.insertWith (++) (_rank l) [k] a) $ level k
+        f :: (Rank,[Kanji]) -> Html ()
+        f (r,ks) = tr_ $ do
+          td_ . toHtml $ show r
+          td_ . toHtml $ map _kanji ks
+
+-- | Yield `Kanji` whose Level isn't known.
+unknowns :: [Kanji] -> [Char]
+unknowns = map _kanji . S.toList . S.filter (not . hasLevel) . S.fromList
